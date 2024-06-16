a31ec6e99754012a42360a1239acec54d78cd9f3
prad1101 <prajapatipradeep1101@gmail.com>
2024-06-16 13:09:24 +0530

    Added username and password in login page

--- app/src/main/java/com/example/androidfirbase/MainActivity.kt
+++ app/src/main/java/com/example/androidfirbase/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidfirbase
 import android.content.Intent
 import android.health.connect.datatypes.units.Length
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val usertxt:EditText= findViewById<EditText>(R.id.txtusername)
+        val userpass:EditText =findViewById<EditText>(R.id.txtpassword)
 
         val login:Button= findViewById<Button>(R.id.loginbtn)
         val linktoregister:TextView= findViewById<TextView>(R.id.signuplink)
@@ -40,9 +42,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         login.setOnClickListener {
-            val uniqueId=usertxt.text.toString();
-            if(uniqueId.isNotEmpty()){
-                    readData(uniqueId)
+            val username=usertxt.text.toString();
+            val userpassword=userpass.text.toString();
+
+            Log.d("data",username);
+            Log.d("data",userpassword);
+
+
+            if(username.isNotEmpty() && userpassword.isNotEmpty()){
+                    readData(username,userpassword)
             }else{
                 Toast.makeText(applicationContext,"User Not Found ",Toast.LENGTH_SHORT).show();
             }
@@ -51,23 +59,33 @@ class MainActivity : AppCompatActivity() {
     }// onCreate Method Over
 
 
-    private fun readData(uniqueId:String){
+    private fun readData(username:String, userpassword:String){
+        Toast.makeText(this,username,Toast.LENGTH_SHORT).show()
         _databaserefrence=FirebaseDatabase.getInstance().getReference("Users")
-        _databaserefrence.child(uniqueId).get().addOnSuccessListener {
+        _databaserefrence.child(username).get().addOnSuccessListener {
             // if user exist or not
             if(it.exists()){
-                // welcome user in your app, with intent and data
-                val username = it.child("name").value;
-                val email    = it.child("email").value;
-                val userid   = it.child("uniqueid").value;
 
-                val intentWelcome = Intent(this, HomeActivity::class.java)
+                val dbpassword=it.child("password").value.toString();
+                if(userpassword==dbpassword){
 
-                intentWelcome.putExtra(KEY1, email.toString())
-                intentWelcome.putExtra(KEY2, username.toString())
-                intentWelcome.putExtra(KEY3, userid.toString())
+                    // welcome user in your app, with intent and data
+                    val username = it.child("name").value;
+                    val email    = it.child("email").value;
+                    val userid   = it.child("uniqueid").value;
 
-                startActivity(intentWelcome)
+
+                    val intentWelcome = Intent(this, HomeActivity::class.java)
+
+                    intentWelcome.putExtra(KEY1, email.toString())
+                    intentWelcome.putExtra(KEY2, username.toString())
+                    intentWelcome.putExtra(KEY3, userid.toString())
+
+                    startActivity(intentWelcome)
+                }else{
+                    Toast.makeText(this,"Password not matched",Toast.LENGTH_SHORT).show()
+                }
+
             }else{
                 Toast.makeText(applicationContext,"User does not exist",Toast.LENGTH_SHORT).show();
             }
